9ca8b61078fe68276d74ffae1babf619a9cb44d2
vipin4948 <vipinkumarcp4@gmail.com>
2021-11-10 20:09:37 +0530

    started database session

---
+++ databases/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Feature
+
+# Create your views here.
+
+def index(request):
+
+    features = Feature.objects.all()
+
+    return render(request,'index.html',{'features': features})
