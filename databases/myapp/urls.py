07f411db0ecd81a97d25d1ddf4d87e3e6295fd1a
vipin4948 <vipinkumarcp4@gmail.com>
2021-11-11 12:03:39 +0530

    done upto dynamic routing of url

--- databases/myapp/urls.py
+++ databases/myapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('login',views.Login,name='login'),
+    path('register',views.register,name='register'),
+    path('logout',views.Logout,name='logout'),
+    path('post/<str:pk>',views.Post,name='post'),
 ]
